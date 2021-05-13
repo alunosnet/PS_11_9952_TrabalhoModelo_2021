86602e36144e991e1299066f8aa73a94f7ee0cc4
Paulo Ferreira <alunosnet@gmail.com>
2021-05-13 10:49:01 +0100

    Aviso dos cookies adicionado

--- PS_11_9952_TrabalhoModelo_2021/MP.Master.cs
+++ PS_11_9952_TrabalhoModelo_2021/MP.Master.cs
@@ -11,14 +11,17 @@ namespace PS_11_9952_TrabalhoModelo_2021
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // verificar se o cookie existe
+            HttpCookie cookie = Request.Cookies["UFCD9952"];
+            if (cookie != null)
+                div_aviso.Visible = false;
         }
         protected void bt1_Click(object sender, EventArgs e)
         {
             //esconder a div
             div_aviso.Visible = false;
             //criar o cookie
-            HttpCookie cookie = new HttpCookie("M7_T2");
+            HttpCookie cookie = new HttpCookie("UFCD9952");
             cookie.Expires = DateTime.Now.AddYears(1);
             Response.Cookies.Add(cookie);
         }
